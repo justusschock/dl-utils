b097a6984be14bcfd2f4569e77529cfcb74e9b95
Justus Schock <justus.schock@rwth-aachen.de>
2020-03-23 11:01:19 +0100

    generalize fake data

--- dlutils/data/fake.py
+++ dlutils/data/fake.py
@@ -1,18 +1,25 @@
 from rising.loading import Dataset
 import torch
+from typing import Union
 
 
-__all__ = ['FakeDictDataset']
+__all__ = ['FakeDataset']
 
-class FakeDictDataset(Dataset):
-    def __init__(self, n_classes: int, shapes: dict, length=5000):
+
+class FakeDataset(Dataset):
+    def __init__(self, n_classes: int, shapes: Union[dict, tuple, list],
+                 length=5000):
         super().__init__()
         self.n_classes = n_classes
         self.shapes = shapes
         self.length = length
 
     def __getitem__(self, item):
-        return {k: torch.rand(shape) for k, shape in self.shapes.items()}
+        if isinstance(self.shapes, dict):
+            return {k: torch.rand(shape) for k, shape in self.shapes.items()}
+        elif isinstance(self.shapes, (tuple, list)):
+            return type(self.shapes)([torch.rand(shape)
+                                      for shape in self.shapes])
 
     def __len__(self):
         return self.length
