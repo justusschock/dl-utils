dae3958ba8b70cf38060bb3e4fe07cd102ab2cc4
Justus Schock <justus.schock@rwth-aachen.de>
2020-03-23 11:11:38 +0100

    update distance loss

--- dlutils/losses/distance.py
+++ dlutils/losses/distance.py
@@ -1,10 +1,13 @@
 import torch
+from typing import Sequence
 from scipy.ndimage.morphology import distance_transform_edt
+from dlutils.utils.tensor_ops import reduce
 
 # TODO: Reimplement distance map transform in torch
 
 __all__ = [
-    'DistanceLoss'
+    'DistanceLoss',
+    'distance_loss'
 ]
 
 
@@ -23,8 +26,30 @@ def get_dist_map(true_hot, in_object=None, factor=-1):
     return distance_map
 
 
+def distance_loss(predictions: torch.Tensor, targets: torch.Tensor,
+                  weight: Sequence = None, reduction: str = 'elementwise_mean'):
+    if weight is None or len(weight) != predictions.shape[1]:
+        weight = torch.ones(predictions.shape[1], device=predictions.device,
+                            dtype=predictions.dtype)
+
+    weight_matrix = torch.stack([torch.ones_like(targets,
+                                                 dtype=weight.dtype) * x
+                                 for x in weight], 1)
+
+    num_classes = predictions.shape[1]
+    true_hot = torch.stack([targets == x
+                            for x in range(num_classes)],
+                           1).to(weight.dtype)
+
+    # for the object factor=-1 to minimize the error on the object
+    # by forcing the net to increase the respective class probability
+    loss = predictions.softmax(dim=1) * get_dist_map(true_hot, factor=-1)
+    loss = loss * weight_matrix / weight.sum()
+    return reduce(loss, reduction=reduction)
+
+
 class DistanceLoss(torch.nn.Module):
-    def __init__(self, weight=None, reduction="mean", **kwargs):
+    def __init__(self, weight=None, reduction="elementwise_mean", **kwargs):
         super().__init__()
 
         if weight is None:
@@ -33,32 +58,6 @@ class DistanceLoss(torch.nn.Module):
             self.register_buffer("weight", torch.tensor(weight))
         self.reduction = reduction
 
-    def forward(self, outputs, targets):
-        # create a hot vector out of the targets for dimension 1
-        # create a hot vector out of the targets for dimension 1
-        weight = self.weight
-        if weight is None or len(weight) != outputs.shape[1]:
-            weight = torch.ones(outputs.shape[1], device=outputs.device,
-                                dtype=outputs.dtype)
-
-        weight_matrix = torch.stack([torch.ones_like(targets,
-                                                     dtype=weight.dtype) * x
-                                     for x in weight], 1)
-
-        num_classes = outputs.shape[1]
-        true_hot = torch.unsqueeze(targets, 1)
-        true_hot = torch.stack([targets == x
-                                for x in range(num_classes)],
-                               1).to(weight.dtype)
-
-        # for the object factor=-1 to minimize the error on the object
-        # by forcing the net to increase the respective class probability
-        loss = outputs.softmax(dim=1) * get_dist_map(true_hot, factor=-1)
-        loss = loss * weight_matrix / weight.sum()
-
-        if self.reduction == 'mean':
-            return loss.mean()
-        elif self.reduction == 'sum':
-            return loss.sum()
-        else:
-            return loss
+    def forward(self, predictions, targets):
+        return distance_loss(predictions=predictions, targets=targets,
+                             weight=self.weight, reduction=self.reduction)
