6c6c0e98ba7ada6e72ccd97a164212becb04f1f6
Justus Schock <justus.schock@rwth-aachen.de>
2020-03-23 10:57:47 +0100

    add fake data

---
+++ dlutils/data/fake.py
@@ -0,0 +1,16 @@
+from rising.loading import Dataset
+import torch
+
+
+class FakeDictDataset(Dataset):
+    def __init__(self, n_classes: int, shapes: dict, length=5000):
+        super().__init__()
+        self.n_classes = n_classes
+        self.shapes = shapes
+        self.length = length
+
+    def __getitem__(self, item):
+        return {k: torch.rand(shape) for k, shape in self.shapes.items()}
+
+    def __len__(self):
+        return self.length
