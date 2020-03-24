22253f23cb617660de69073069823f968a61ac77
Justus Schock <justus.schock@rwth-aachen.de>
2020-03-24 11:08:18 +0100

    docstrings

--- dlutils/losses/distance.py
+++ dlutils/losses/distance.py
@@ -11,7 +11,21 @@ __all__ = [
 ]
 
 
-def get_dist_map(true_hot, in_object=None, factor=-1):
+def _get_dist_map(true_hot: torch.Tensor, factor: float = -1) -> torch.Tensor:
+    """
+    Helper function to calculate the actual distance map
+    Parameters
+    ----------
+    true_hot : torch.Tensor
+        the onehot encoded target image
+    factor : float
+        scaling factor
+
+    Returns
+    -------
+    torch.Tensor
+        the calculated distance map
+    """
     distance_map = torch.zeros_like(true_hot)
 
     # calculates the euclidean distance map for each image and label
@@ -20,14 +34,34 @@ def get_dist_map(true_hot, in_object=None, factor=-1):
         for j in range(true_hot.shape[1]):
             distance_map[i, j] = torch.tensor(
                 distance_transform_edt((true_hot[i, j] != 1).cpu().numpy()))
-            # distance_map[i, j][true_hot[i, j] == 1] = in_object[j]
             distance_map[i, j][true_hot[i, j] == 1] = factor * distance_map[i, j].max()
 
     return distance_map
 
 
 def distance_loss(predictions: torch.Tensor, targets: torch.Tensor,
-                  weight: Sequence = None, reduction: str = 'elementwise_mean'):
+                  weight: Sequence = None,
+                  reduction: str = 'elementwise_mean') -> torch.Tensor:
+    """
+    Calculates the distance loss
+
+    Parameters
+    ----------
+    predictions : torch.Tensor
+        the predicted segmentations
+    targets : torch.Tensor
+        the groundtruth segmentations
+    weight : Sequence
+        optional weights
+    reduction : str
+        what kind of reduction to apply
+
+    Returns
+    -------
+    torch.Tensor
+        the loss value
+
+    """
     if weight is None or len(weight) != predictions.shape[1]:
         weight = torch.ones(predictions.shape[1], device=predictions.device,
                             dtype=predictions.dtype)
@@ -43,13 +77,24 @@ def distance_loss(predictions: torch.Tensor, targets: torch.Tensor,
 
     # for the object factor=-1 to minimize the error on the object
     # by forcing the net to increase the respective class probability
-    loss = predictions.softmax(dim=1) * get_dist_map(true_hot, factor=-1)
+    loss = predictions.softmax(dim=1) * _get_dist_map(true_hot, factor=-1)
     loss = loss * weight_matrix / weight.sum()
     return reduce(loss, reduction=reduction)
 
 
 class DistanceLoss(torch.nn.Module):
-    def __init__(self, weight=None, reduction="elementwise_mean", **kwargs):
+    def __init__(self, weight=None, reduction="elementwise_mean"):
+        """
+        Modular interface to the distance loss
+
+        Parameters
+        ----------
+        weight : Sequence
+            optional weights
+        reduction : str
+            what kind of reduction to apply
+        """
+
         super().__init__()
 
         if weight is None:
@@ -58,6 +103,22 @@ class DistanceLoss(torch.nn.Module):
             self.register_buffer("weight", torch.tensor(weight))
         self.reduction = reduction
 
-    def forward(self, predictions, targets):
+    def forward(self, predictions, targets) -> torch.Tensor:
+        """
+        Calculates the actual loss value
+
+        Parameters
+        ----------
+        predictions : torch.Tensor
+            the predicted segmentations
+        targets : torch.Tensor
+            the groundtruth segmentations
+
+        Returns
+        -------
+        torch.Tensor
+            loss value
+
+        """
         return distance_loss(predictions=predictions, targets=targets,
                              weight=self.weight, reduction=self.reduction)
