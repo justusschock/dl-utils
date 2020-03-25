6b58213768a7b5ede0a59637a104b0c41c87379f
Justus Schock <justus.schock@rwth-aachen.de>
2020-03-25 10:07:24 +0100

    add nd alexnet

---
+++ dlutils/models/alexnet.py
@@ -0,0 +1,41 @@
+from dlutils.models.nd import ConvNd, PoolingNd
+import torch
+
+
+class AlexNet(torch.nn.Module):
+    def __init__(self, num_classes: int, in_channels: int, n_dim: int = 2,
+                 pool_type: str = "Max"):
+        super().__init__()
+        self.features = torch.nn.Sequential(
+            ConvNd(n_dim, in_channels, 64, kernel_size=11, stride=4,
+                   padding=2),
+            torch.nn.ReLU(inplace=True),
+            PoolingNd(pool_type, n_dim, kernel_size=3, stride=2),
+            ConvNd(n_dim, 64, 192, kernel_size=5, padding=2),
+            torch.nn.ReLU(inplace=True),
+            PoolingNd(pool_type, n_dim, kernel_size=3, stride=2),
+            ConvNd(n_dim, 192, 384, kernel_size=3, padding=1),
+            torch.nn.ReLU(inplace=True),
+            ConvNd(n_dim, 384, 256, kernel_size=3, padding=1),
+            torch.nn.ReLU(inplace=True),
+            ConvNd(n_dim, 256, 256, kernel_size=3, padding=1),
+            torch.nn.ReLU(inplace=True),
+            PoolingNd(pool_type, n_dim, kernel_size=3, stride=2),
+        )
+        self.avgpool = PoolingNd("AdaptiveAvg", n_dim, 6)
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Dropout(),
+            torch.nn.Linear(256 * pow(6, n_dim), 4096),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Dropout(),
+            torch.nn.Linear(4096, 4096),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, input_batch: torch.Tensor) -> torch.Tensor:
+        features = self.features(input_batch)
+        pooled = self.avgpool(features)
+        flattened = pooled.view(input_batch.size(0), -1)
+        return self.classifier(flattened)
+
