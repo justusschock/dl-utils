f3ec398ec58b783a2a2ffddfcd08fc92301b1325
Justus Schock <justus.schock@rwth-aachen.de>
2020-03-25 12:31:09 +0100

    bs loss

---
+++ dlutils/losses/boundary_seeking.py
@@ -0,0 +1,34 @@
+import torch
+class BoundarySeekingLoss(torch.nn.Module):
+    """
+    Boundary Seeking Loss
+    References
+    ----------
+    https://wiseodd.github.io/techblog/2017/03/07/boundary-seeking-gan/
+    """
+
+    def __init__(self, weight=0.5):
+        """
+        Parameters
+        ----------
+        weight : float
+            weighting factor
+        """
+        super().__init__()
+
+        self._weight = weight
+
+    def forward(self, pred):
+        """
+        Calculates the actual loss
+        Parameters
+        ----------
+        pred : :class:`torch.Tensor`
+            the prediction (typically obtained by the discriminator)
+        Returns
+        -------
+        :class:`torch.Tensor`
+            the loss value
+        """
+        return self._weight * torch.mean((torch.log(pred) -
+                                          torch.log(1 - pred)) ** 2)
